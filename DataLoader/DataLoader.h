cf3d0ef43bde35cccb2b56481bbc381284124c8e
Luciano <passos.luciano@outlook.com>
2017-02-01 22:56:28 -0200

    Initial Commit

---
+++ DataLoader/DataLoader.h
@@ -0,0 +1,19 @@
+//
+//  DataLoader.h
+//  DataLoader
+//
+//  Created by Luciano Almeida on 01/02/17.
+//  Copyright © 2017 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataLoader.
+FOUNDATION_EXPORT double DataLoaderVersionNumber;
+
+//! Project version string for DataLoader.
+FOUNDATION_EXPORT const unsigned char DataLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataLoader/PublicHeader.h>
+
+
